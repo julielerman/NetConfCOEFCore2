b6298df339bdc30711a675d0e30e6a197c9b6d0d
julie <julielerman@gmail.com>
2017-10-29 22:17:33 -0400

    add program.cs

---
+++ program.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using DataModel;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Console {
+  class Program {
+    static void Main (string[] args) {
+      using (var context = new SamuraiContext ()) {
+        context.Database.EnsureDeleted ();
+        context.Database.Migrate ();
+      }
+      //StoreNewSamuraiWithEntrance ();
+      //StoreNewSamuraiWithEntranceAndIdentity();
+      //ListSamuraisWithEntranceAndIdentity();
+    
+     }
+    
+    // static void StoreNewSamuraiWithEntrance () {
+    //   var samurai = new Samurai { Name = "Julie" };
+
+    //   samurai.CreateEntrance (1, "S1", "Here I am!");
+    //   using (var context = new SamuraiContext ()) {
+    //     context.Samurais.Add (samurai);
+    //     context.SaveChanges ();
+    //   }
+    // }
+
+//     static void StoreNewSamuraiWithEntranceAndIdentity () {
+//       var samurai = new Samurai { Name = "Giantpuppy" };
+// samurai.Identify("Sampson","Newfie");
+//       samurai.CreateEntrance (2, "S2", "Woof!");
+//       using (var context = new SamuraiContext ()) {
+//         context.Samurais.Add (samurai);
+//         context.SaveChanges ();
+//       }
+//     }
+    // static void ListSamuraisWithEntranceAndIdentity () {
+    //   using (var context = new SamuraiContext ()) {
+    //     var samurais = context.Samurais.Include ("Entrance").ToList ();
+    //     foreach (var samurai in samurais) {
+    //       Console.WriteLine ($"{samurai.Name}, Enters in {samurai.EntranceScene} ");
+    //       Console.WriteLine ($"Secret Identity: {samurai.SecretIdentity.FullName()}");
+
+    //     }
+    //   }
+    // }
+       
+
+  }
+}
