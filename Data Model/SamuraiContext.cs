f99e64c58d263f9c2996696eba00f8a9b86f67e6
julie <julielerman@gmail.com>
2017-10-29 21:59:00 -0400

    initial

---
+++ Data Model/SamuraiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+
+namespace DataModel
+{
+    public class SamuraiContext:DbContext 
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=VegasSamurai.db");
+             base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ DomainModel/Samurai.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SamuraiApp.Domain {
+  public class Samurai {
+
+    public Samurai () {
+      Quotes = new List<Quote> ();
+        }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Quote> Quotes { get; set; } //1:*
+
+    public Entrance Entrance { get; set; } //1:1
+    
+    
+  }
+}
