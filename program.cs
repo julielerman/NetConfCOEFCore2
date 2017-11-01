1655202f2c1480851502d0a2e1a377106aa82a81
julie <julielerman@gmail.com>
2017-11-01 07:03:11 -0700

    fix extra paren

--- program.cs
+++ program.cs
@@ -67,7 +67,7 @@ namespace SamuraiApp.Console
       // {
       //   var samurai=context.Samurais.Find(1);
       //   samurai.AddQuote("hello? hello?");
-      //   var quote=Samurai.AddQuote("All those apples will make you sick, silly boy", 2));
+      //   var quote=Samurai.AddQuote("All those apples will make you sick, silly boy", 2);
       //   context.Add(quote);
       //   context.SaveChanges();
       // }
