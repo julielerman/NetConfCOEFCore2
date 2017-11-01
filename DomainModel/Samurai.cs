79cb557cb49eb1d3f59075a76409c1248ceac8c9
julie <julielerman@gmail.com>
2017-10-31 21:23:52 -0700

    putting secret identity iinto starting point

--- DomainModel/Samurai.cs
+++ DomainModel/Samurai.cs
@@ -14,6 +14,9 @@ namespace SamuraiApp.Domain {
 
     public Entrance Entrance { get; set; } //1:1
     
-    
+    public PersonName SecretIdentity { get; private set; }
+    public void Identify (string first, string last) {
+     SecretIdentity = PersonName.Create (first, last);
+    }
   }
 }
