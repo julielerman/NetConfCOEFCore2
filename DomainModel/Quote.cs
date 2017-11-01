7cc278b693a6b6fbf872a74babe8d2397bd3f376
julie <julielerman@gmail.com>
2017-10-31 21:34:56 -0700

    fix up quote

--- DomainModel/Quote.cs
+++ DomainModel/Quote.cs
@@ -1,7 +1,14 @@
-﻿namespace SamuraiApp.Domain {
-  public class Quote {
-    public int Id { get; set; }
+﻿namespace SamuraiApp.Domain
+{
+  public class Quote
+  {
+    public Quote(string quoteText, int samuraiId)
+    {
+      Text = quoteText;
+      SamuraiId = samuraiId;
+    }
+    public int Id { get; private set; }
     public string Text { get; set; }
-    public int SamuraiId { get; set; }
+    public int SamuraiId { get; private set; }
   }
 }
