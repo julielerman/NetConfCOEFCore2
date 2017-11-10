0d86b6e2f3b4afa2ca965bebfaa46db24915fba0
julie <julielerman@gmail.com>
2017-11-10 18:22:43 -0500

    db name

--- Data Model/SamuraiContext.cs
+++ Data Model/SamuraiContext.cs
@@ -1,23 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
-using SamuraiApp.Domain;
-using System;
+﻿using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
 
-namespace DataModel
-{
-    public class SamuraiContext:DbContext 
-    {
+namespace DataModel {
+    public class SamuraiContext : DbContext {
         public DbSet<Samurai> Samurais { get; set; }
         private DbSet<Quote> Quote { get; set; }
-protected override void OnModelCreating(ModelBuilder modelBuilder)
-{
-   modelBuilder.Entity<Samurai>().HasOne(typeof(Entrance),"Entrance");
-   base.OnModelCreating(modelBuilder);
-}
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Filename=VegasSamurai.db");
+        protected override void OnModelCreating (ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Samurai> ().HasOne (typeof (Entrance), "Entrance");
+            base.OnModelCreating (modelBuilder);
         }
-    
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlite ("Filename=MedellinSamurai.db");
+        }
+
     }
 }
