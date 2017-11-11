449ec1eb660e0061c4765a5dfa67b3e4d8d26048
julie <julielerman@gmail.com>
2017-11-11 09:19:44 -0500

    change file path

--- Data Model/SamuraiContext.cs
+++ Data Model/SamuraiContext.cs
@@ -11,7 +11,7 @@ namespace DataModel
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Filename=Samurai.db");
+            optionsBuilder.UseSqlite("Filename=../DemoData/Samurai.db");
         }
     
     }
