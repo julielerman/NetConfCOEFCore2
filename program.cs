af66398a4ca073333a6eb8bbd63a95a24cf901dc
julie <julielerman@gmail.com>
2017-11-11 09:23:58 -0500

    rename method

--- program.cs
+++ program.cs
@@ -13,13 +13,13 @@ namespace SamuraiApp.Console {
       }
       StoreNewSamuraiWithEntrance ();
       StoreNewSamuraiWithEntranceAndQuote ();
-      AddQuoteToSamurai ();
+      AddQuoteToExistingSamurai ();
     }
 
     static void StoreNewSamuraiWithEntrance () {
       var samurai = new Samurai ("Kojashi2");
      //samurai.Entrance = new Entrance (1, "Scene 1", "Caminando por una carretera comiendo una manzana");
-      samurai.CreateEntrance (1, "Scene 2", "Caminando por una carretera comiendo una manzana");
+       samurai.CreateEntrance (1, "Scene 2", "Caminando por una carretera comiendo una manzana");
       using (var context = new SamuraiContext ()) {
         context.Samurais.Add (samurai);
         context.SaveChanges ();
@@ -38,7 +38,7 @@ namespace SamuraiApp.Console {
       }
     }
 
-    static void AddQuoteToSamurai () {
+    static void AddQuoteToExistingSamurai () {
       using (var context = new SamuraiContext ()) {
         //var samurai=context.Samurais.Find(1);
         //samurai.AddQuote("hello? hello?");
